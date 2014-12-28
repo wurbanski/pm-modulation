24db36fcfb39e058dc166b7c3cc0c183c8169b6c
Wojciech Urbanski <teh.urb@gmail.com>
2014-12-28 19:26:48 +0100

    Many different changes

--- devices.py
+++ devices.py
@@ -1,10 +1,14 @@
-import math
-
-__author__ = 'Wojciech'
+__author__ = 'Wojciech Urbański'
 
 import numpy as np
 import scipy as sp
 import numpy.fft as fft
+import matplotlib.pyplot as plt
+
+
+def freqplot(signal_array, rate):
+    freqline = np.arange(-rate/2, rate/2, rate/len(signal_array))
+    plt.plot(freqline, signal_array)
 
 
 class SystemConfiguration():
@@ -12,23 +16,33 @@ class SystemConfiguration():
 
     def __init__(self, time=1, rate=1):
         self.timeline = np.arange(0, time, 1/rate)
+        self.time = time
+        self.rate = rate
 
     def add_block(self, block, position=-1):
-        if -1 > position:
-            self.blocks.insert(position, block)
+        if isinstance(block, Block):
+            if -1 > position:
+                self.blocks.insert(position, block)
+            else:
+                self.blocks.append(block)
         else:
-            self.blocks.append(block)
+            raise TypeError("Specified element is not of 'Block' type")
 
     def list_blocks(self):
         print('Total blocks: ', len(self.blocks))
         for block in self.blocks:
-            print('Block ', self.blocks.index(block), ': ', block.name(), sep='')
+            print('Block ', self.blocks.index(block), ': ', block.name, sep='')
 
     def refresh_blocks(self):
         for i in range(1, len(self.blocks)):
-            print(i - 1, self.blocks[i - 1].name(), 'to', i, self.blocks[i].name())
-            self.blocks[i].input(self.blocks[i - 1])
+            print(i - 1, self.blocks[i - 1].name, 'to', i, self.blocks[i].name)
+            self.blocks[i].connect(self.blocks[i - 1])
 
+    def get_block(self, i):
+        if i < len(self.blocks):
+            return self.blocks[i]
+        else:
+            raise ValueError("No block of specified type exists.")
 
 
 class Signal():
@@ -36,7 +50,7 @@ class Signal():
         self.signal = signal_array
 
     def get_fft(self):
-        spectrum = fft.fftshift(fft.fft(self.signal * np.blackman(np.len(self.signal))))
+        spectrum = fft.fftshift(fft.fft(self.signal * np.blackman(len(self.signal))))
         return spectrum / np.max(np.abs(spectrum))
 
     def get_energy(self):
@@ -44,64 +58,90 @@ class Signal():
 
 
 class Block():
-    def __init__(self, config):
+    def __init__(self, config, name="Generic Block"):
         self.config = config
-        self.signal_in = Signal(np.zeros(self.config.timeline.shape))
-        self.signal_out = self.process(self.signal_in)
+        self.__signal_in = Signal(np.zeros(self.config.timeline.shape))
+        self.__signal_out = self.process(self.__signal_in)
+        self.__name = name
 
-    def input(self, previous_block):
-        self.signal_in = previous_block.output()
-        self.signal_out = self.process(self.signal_in)
+    def connect(self, previous_block):
+        if isinstance(previous_block, Block):
+            self.__signal_in = previous_block.output
+            self.__signal_out = self.process(self.__signal_in)
+        else:
+            raise TypeError("Wrong type, cannot connect.")
 
     def process(self, signal_in):
-        return signal_in
+        pass
 
+    @property
+    def input(self):
+        return self.__signal_in
+
+    @property
     def output(self):
-        return self.signal_out
+        return self.__signal_out
 
+    @property
     def name(self):
-        return "Generic block"
+        return self.__name
 
 
 class SineInputBlock(Block):
-    def __init__(self, config, frequency=1, amplitude=1):
-        self.frequency = frequency
-        self.amplitude = amplitude
-        super().__init__(config)
+    def __init__(self, config, frequency=1, amplitude=1, name="Sine Input"):
+        self._frequency = frequency
+        self._amplitude = amplitude
+        super().__init__(config, name)
 
     def process(self, signal_in):
-        return Signal(self.amplitude * np.sin(self.config.timeline * self.frequency))
-
-    def name(self):
-        return "SineInput Block"
+        return Signal(self._amplitude * np.sin(self.config.timeline * self._frequency))
 
 
 class PhaseModulatorBlock(Block):
-    def __init__(self, config, frequency=1, amplitude=1):
+    def __init__(self, config, frequency=1, amplitude=1, name="Phase Modulator"):
         self.frequency = frequency
         self.amplitude = amplitude
-        super().__init__(config)
+        super().__init__(config, name)
 
     def process(self, signal_in):
         return Signal(self.amplitude * np.sin(self.frequency * self.config.timeline + signal_in.signal))
 
-    def name(self):
-        return "PhaseModulator Block"
-
 
 class AWGNChannelBlock(Block):
-    def __init__(self, config, snr=20):
-        self.snr = snr
-        super().__init__(config)
+    def __init__(self, config, snr=20, name="AWGN Channel"):
+        self._snr = snr
+        super().__init__(config, name)
 
     def process(self, signal_in):
         var = signal_in.get_energy()
         if var > 0:
-            sigma = np.sqrt(var) * 10 ** (-self.snr / 20)
+            sigma = np.sqrt(var) * 10 ** (-self._snr / 20)
             noise = np.random.normal(loc=0.0, scale=sigma, size=self.config.timeline.shape)
             return Signal(signal_in.signal + noise)
         else:
             return signal_in
 
-    def name(self):
-        return "AWGNChannel Block"
+
+class BandPassFilterBlock(Block):
+    def __init__(self, config, bot_freq=1, top_freq=10, name="Band Pass Filter"):
+        self.low_freq = bot_freq
+        self.hi_freq = top_freq
+        super().__init__(config, name)
+        self.__name = "BandPass Filter (%d - %d)" % (self.low_freq, self.hi_freq)
+
+    def process(self, signal_in):
+        pass
+
+# bandwidth of PM signal = 2 * (<max_freq_deviation>+<max_modulator_freq>)
+# max_freq_deviation = np.max(mod_ampl*np.diff(signal)
+# hi_freq = <carrier_freq> + BW/2
+# low_freq = <carrier_freq> - BW/2
+
+
+class PhaseDemodulatorBlock(Block):
+    def __init__(self, config, name="Phase Demodulator"):
+        super().__init__(config, name)
+
+    def process(self, signal_in):
+
+        square_signal = signal_in(signal_in > 0.5)
--- test.py
+++ test.py
@@ -1,28 +1,29 @@
 __author__ = 'Wojciech Urbański'
 
 import devices
+from devices import freqplot
 import matplotlib.pyplot as plt
 
 print("Setting up the system...")
 config = devices.SystemConfiguration(1, 100)
 
 print("Setting up InputBlock...")
-input_block = devices.SineInputBlock(config, 10, 1)
-print(input_block.name())
+input_block = devices.SineInputBlock(config, 1, 10)
+print(input_block.name)
 
 print("Adding block to the system...")
 config.add_block(input_block)
 
 print("Setting up PhaseModulatorBlock")
-pm_block = devices.PhaseModulatorBlock(config, 10, 1)
-print(pm_block.name())
+pm_block = devices.PhaseModulatorBlock(config, 1, 10)
+print(pm_block.name)
 
 print("Adding block to the system...")
 config.add_block(pm_block)
 
 print("Setting up AWGNChannelBlock")
 awgn_block = devices.AWGNChannelBlock(config)
-print(awgn_block.name())
+print(awgn_block.name)
 
 print("Adding block to the system...")
 config.add_block(awgn_block)
@@ -35,17 +36,24 @@ print("Connecting Blocks...")
 config.refresh_blocks()
 
 print("Plotting input...")
-plt.plot(config.blocks[0].signal_out.signal)
+plt.plot(config.blocks[0].output.signal)
 plt.show()
 plt.close()
 print("...done")
-plt.plot(config.blocks[1].signal_in.signal)
+plt.plot(config.blocks[1].input.signal)
 plt.hold(True)
-plt.plot(config.blocks[1].output().signal)
+plt.plot(config.blocks[1].output.signal)
 plt.show()
 plt.close()
 print("Plotting output...")
-plt.plot(config.blocks[-1].output().signal)
+plt.plot(config.blocks[-1].output.signal)
 plt.show()
 plt.close()
 print("...done")
+freqplot(config.blocks[1].output.get_fft(), config.rate)
+plt.hold(True)
+freqplot(config.blocks[-1].output.get_fft(), config.rate)
+plt.show()
+plt.close()
+
+
